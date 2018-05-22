c8dfdeefeec2239753f29e477e5c2fbee3164fc0
Dc520134 <dc_1419@163.com>
2018-05-22 12:34:02 +0800

    搭建eureka微服务注册中心

---
+++ eureka-server/src/main/java/com/kerry/cloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.kerry.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 微服务注册中心:netflix-eureka
+ *
+ * @author Kerry Dong
+ * @date 2018/05/22
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
