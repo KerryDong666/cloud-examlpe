9ccc839f4191c202912197fb53bacd7c0ffd0a12
Dc520134 <dc_1419@163.com>
2018-05-22 16:16:37 +0800

    微服务实例goods-server作为eureka-client注册到eureka-server(微服务注册中心)

---
+++ eureka-client-user-server/src/main/java/com/kerry/cloud/eureka_client/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.kerry.cloud.eureka_client.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author CP_dongchuan
+ * @date 2018/5/22
+ */
+@RestController
+public class TestController {
+
+	@RequestMapping("/test")
+	public String test(){
+		return "this is user server";
+	}
+
+}
